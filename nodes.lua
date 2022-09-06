bebf9dcc3df2ba48e0ee7ec31f48b4e2e7b7593c
Samuel Matzko <matzko+sam@gmail.com>
2022-09-06 18:47:45 -0400

    ✨ Finished adding the stones.

--- nodes.lua
+++ nodes.lua
@@ -1,9 +1,21 @@
 -- one-way glass
 local oneway_glass = {
     {"cobblestone", "Cobblestone", "default_cobble.png"},
+    {"desert_sandstone", "Desert Sandstone", "default_desert_sandstone.png"},
+    {"desert_standstone_block", "Desert Sandstone Block", "default_desert_sandstone_block.png"},
+    {"desert_sandstone_brick", "Desert Sandstone Brick", "default_desert_sandstone_brick.png"},
+    {"desert_stone", "Desert Stone", "default_desert_stone.png"},
+    {"desert_stone_block", "Desert Stone Block", "default_desert_stone_block.png"},
+    {"desert_stone_brick", "Desert Stone Brick", "default_desert_stone_brick.png"},
     {"mossy_cobblestone", "Mossy Cobblestone", "default_mossycobble.png"},
+    {"sandstone", "Sandstone", "default_sandstone.png"},
+    {"sandstone_block", "Sandstone Block", "default_sandstone_block.png"},
+    {"sandstone_brick", "Sandstone Brick", "default_sandstone_brick.png"},
+    {"silver_sandstone", "Silver Sandstone", "default_silver_sandstone.png"},
+    {"silver_sandstone_block", "Silver Sandstone Block", "default_silver_sandstone_block.png"},
     {"stone", "Stone", "default_stone.png"},
     {"stone_block", "Stone Block", "default_stone_block.png"},
+    {"stone_brick", "Stone Brick", "default_stone_brick.png"},
 }
 
 for i in ipairs(oneway_glass) do
